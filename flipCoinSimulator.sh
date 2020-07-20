4d64c42c004ef1aef11f6392fd26370ae2f440b2
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-20 11:17:19 +0530

    UC2-Flip-Multiple-Times-Display-Head-Tail-Count implemented

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,33 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo "Welcome to Flip Coin Simulator Problem"
 
+echo "Flipping a coin one time"
 result=$(($(($RANDOM%10))%2))
 if [ $result -eq 1 ]
 then
-	echo "Heads"
+	echo "Heads Won"
 else
-	echo "Tails"
+	echo "Tails Won"
 fi
+
+echo "Flipping a coin multiple times"
+i=1
+count1=0
+count2=0
+
+while [ $i -le 20 ]
+do
+	result1=$(($(($RANDOM%10))%2))
+	if [ $result1 -eq 1 ]
+	then
+		count1=$(($count1+1))
+		((i++))
+	else
+		count2=$(($count2+1))
+		((i++))
+	fi
+done
+
+echo "Heads won " $count1 " times"
+echo "Tails won " $count2 " times"
